718970f554fd3522d57d0e60896c0d7bc880920e
Matt <qqwweeaassddzzzxxxccc111@gmail.com>
2020-05-28 06:21:10 -0400

    Revert "interface files"
    This reverts commit f5598bcdfc2c27d849f8bc1a5f0785e1c3e9bac1.

--- Sources/CyMapInterface.cpp
+++ Sources/CyMapInterface.cpp
@@ -15,9 +15,13 @@ void CyMapPythonInterface()
 /***** Parallel Maps - Begin *****/
 /*********************************/
 		.def("getType", &CyMap::getType, "int ()")
+		.def("setType", &CyMap::setType, "void (MapTypes eNewType)")
 
+		.def("viewportsEnabled", &CyMap::viewportsEnabled, "bool ()")
 		.def("getViewportWidth", &CyMap::getViewportWidth, "int ()")
 		.def("getViewportHeight", &CyMap::getViewportHeight, "int ()")
+		.def("getMapXFromViewportX", &CyMap::getMapXFromViewportX, "int (int iX)")
+		.def("getMapYFromViewportY", &CyMap::getMapYFromViewportY, "int (int iY)")
 		.def("getViewportXFromMapX", &CyMap::getViewportXFromMapX, "int (int iX)")
 		.def("getViewportYFromMapY", &CyMap::getViewportYFromMapY, "int (int iY)")
 		.def("isInViewport", &CyMap::isInViewport, "bool (int iX, int iY)")
@@ -28,6 +32,8 @@ void CyMapPythonInterface()
 /***** Parallel Maps - End *****/
 /*******************************/
 
+		.def("erasePlots", &CyMap::erasePlots, "() - erases the plots")
+		.def("setRevealedPlots", &CyMap::setRevealedPlots, "void (int /*TeamTypes*/ eTeam, bool bNewValue, bool bTerrainOnly) - reveals the plots to eTeam")
 		.def("resetRevealedPlots", &CyMap::resetRevealedPlots, "void (int /*TeamTypes*/ eTeam) - removes reveal status on all plots to eteam but then resets reveal for that plot if is currently visible")
 		.def("setAllPlotTypes", &CyMap::setAllPlotTypes, "void (int /*PlotTypes*/ ePlotType) - sets all plots to ePlotType")
 
@@ -40,13 +46,20 @@ void CyMapPythonInterface()
 /************************************************************************************************/
 /* REVOLUTION_MOD                          END                                                  */
 /************************************************************************************************/
+		.def("updateVisibility", &CyMap::updateVisibility, "() - updates the plots visibility")
+		.def("syncRandPlot", &CyMap::syncRandPlot, python::return_value_policy<python::manage_new_object>(), "CyPlot* (iFlags,iArea,iMinUnitDistance,iTimeout) - random plot based on conditions")
 		.def("findCity", &CyMap::findCity, python::return_value_policy<python::manage_new_object>(), "CyCity* (int iX, int iY, int (PlayerTypes) eOwner = NO_PLAYER, int (TeamTypes) eTeam = NO_TEAM, bool bSameArea = true, bool bCoastalOnly = false, int (TeamTypes) eTeamAtWarWith = NO_TEAM, int (DirectionTypes) eDirection = NO_DIRECTION, CvCity* pSkipCity = NULL) - finds city")
+		.def("findSelectionGroup", &CyMap::findSelectionGroup, python::return_value_policy<python::manage_new_object>(), "CvSelectionGroup* (int iX, int iY, int /*PlayerTypes*/ eOwner, bool bReadyToSelect, bool bWorkers)")
 
 		.def("findBiggestArea", &CyMap::findBiggestArea, python::return_value_policy<python::manage_new_object>(), "CyArea* ()")
 
 		.def("getMapFractalFlags", &CyMap::getMapFractalFlags, "int ()")
+		.def("findWater", &CyMap::findWater, "bool (CyPlot* pPlot, int iRange, bool bFreshWater)")
+		.def("isPlot", &CyMap::isPlot, "bool (iX,iY) - is (iX, iY) a valid plot?")
 		.def("numPlots", &CyMap::numPlots, "int () - total plots in the map")
 		.def("plotNum", &CyMap::plotNum, "int (iX,iY) - the index for a given plot") 
+		.def("plotX", &CyMap::plotX, "int (iIndex) - given the index of a plot, returns its X coordinate") 
+		.def("plotY", &CyMap::plotY, "int (iIndex) - given the index of a plot, returns its Y coordinate") 
 		.def("getGridWidth", &CyMap::getGridWidth, "int () - the width of the map, in plots")
 		.def("getGridHeight", &CyMap::getGridHeight, "int () - the height of the map, in plots")
 
@@ -76,6 +89,7 @@ void CyMapPythonInterface()
 		.def("sPlotByIndex", &CyMap::sPlotByIndex, python::return_value_policy<python::reference_existing_object>(), "CyPlot (iIndex) - static - get plot by iIndex")
 		.def("plot", &CyMap::plot, python::return_value_policy<python::manage_new_object>(), "CyPlot (iX,iY) - get CyPlot at (iX,iY)")
 		.def("sPlot", &CyMap::sPlot, python::return_value_policy<python::reference_existing_object>(), "CyPlot (iX,iY) - static - get CyPlot at (iX,iY)")
+		.def("pointToPlot", &CyMap::pointToPlot, python::return_value_policy<python::manage_new_object>())
 		.def("getIndexAfterLastArea", &CyMap::getIndexAfterLastArea, "int () - index for handling NULL areas")
 		.def("getNumAreas", &CyMap::getNumAreas, "int () - total areas")
 		.def("getNumLandAreas", &CyMap::getNumLandAreas, "int () - total land areas")
@@ -85,7 +99,10 @@ void CyMapPythonInterface()
 
 		.def("calculatePathDistance", &CyMap::calculatePathDistance, "finds the shortest passable path between two CyPlots and returns its length, or returns -1 if no such path exists. Note: the path must be all-land or all-water")
 		.def("rebuild", &CyMap::rebuild, "used to initialize the map during WorldBuilder load")
+		.def("regenerateGameElements", &CyMap::regenerateGameElements, "used to regenerate everything but the terrain and height maps")
+		.def("updateFog", &CyMap::updateFog, "void ()")
 		.def("updateMinimapColor", &CyMap::updateMinimapColor, "void ()")
+		.def("updateMinOriginalStartDist", &CyMap::updateMinOriginalStartDist, "void (CyArea* pArea)")
 
 		// AIAndy: Expose path generation functionality to Python
 		.def("generatePathForHypotheticalUnit", &CyMap::generatePathForHypotheticalUnit, "bool (CyPlot*, CyPlot*, int, int, int, int)")
@@ -97,10 +114,13 @@ void CyMapPythonInterface()
 /*                                                                                              */
 /*                                                                                              */
 /************************************************************************************************/
+		.def("firstArea", &CyMap::firstArea, "tuple(CyArea, int iterOut) (bool bReverse) - gets the first area")
+		.def("nextArea", &CyMap::nextArea, "tuple(CyArea, int iterOut) (int iterIn, bool bReverse) - gets the next area")
 /************************************************************************************************/
 /* Afforess	                     END                                                            */
 /************************************************************************************************/
 		// Super Forts begin *canal* *choke*
+		.def("calculateCanalAndChokePoints", &CyMap::calculateCanalAndChokePoints, "void ()")
 		// Super Forts end
 		;
 }
